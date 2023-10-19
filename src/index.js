d59554cf9f488807909a64e839c2763193ab120d
Anton Listkov <antonlistkov@gmail.com>
2023-10-19 22:24:36 +0200

    finish comparison of json files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
-export default (path1, path2) => {
-  const data1 = fs.readFileSync(path.resolve(path1), 'utf-8');
-  const data2 = fs.readFileSync(path.resolve(path2), 'utf-8');
-  console.log(data1, data2);
+const readFile = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf8');
+
+const gendiff = (path1, path2) => {
+  const data1 = JSON.parse(readFile(path1));
+  const data2 = JSON.parse(readFile(path2));
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const uniqKeys = _.uniq(keys1, keys2);
+  const sortedKeys = uniqKeys.sort();
+
+  const result = sortedKeys.flatMap((key) => {
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] === data2[key]) {
+        return `${key}: ${data1[key]}`;
+      } else {
+        return [`-${key}: ${data1[key]}`, `+${key}: ${data2[key]}`];
+      }
+    }
+
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return `-${key}: ${data1[key]}`;
+    }
+
+    return `+${key}: ${data2[key]}`;
+  });
+  return `{\n  ${result.join('\n  ')}\n}`;
 };
+
+export default gendiff;
