2606c043b8cd2d84589023b083e160a65a67774b
Anton Listkov <antonlistkov@gmail.com>
2023-10-23 23:53:56 +0200

    fix code

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import _ from 'lodash';
 
 const readFile = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf8');
 
+const getDataAndKeys = (path) => {
+  const data = JSON.parse(readFile(path));
+  const keys = Object.keys(data);
+  return [data, keys];
+};
+
 const gendiff = (path1, path2) => {
-  const data1 = JSON.parse(readFile(path1));
-  const data2 = JSON.parse(readFile(path2));
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
+  const [data1, keys1] = getDataAndKeys(path1);
+  const [data2, keys2] = getDataAndKeys(path2);
   const uniqKeys = _.uniq([...keys1, ...keys2]);
   const sortedKeys = uniqKeys.sort();
 
